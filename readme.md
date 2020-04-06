736232511b0259d0b2faf0d889f359b75eae1a36
Shenghan Gao <gaoshenghan199123@gmail.com>
2020-04-06 02:35:22 -0700

    finished scrapping today

---
+++ readme.md
@@ -0,0 +1,1 @@
+# A Web Crawler using scrapy to scrap titles of pauuu.cn
--- spider.py
+++ spider.py
@@ -1,12 +1,19 @@
 import scrapy
 
 class BlogSpider(scrapy.Spider):
-    name = 'blogspider'
-    start_urls = ['http://pauuu.cn/']
-
+    name = 'uuspider'
+    start_urls = ['http://pauuu.cn/nr.php?id=0']
+    selector = (
+        'body > div.details.clearfix > div >'
+        'div.d-left > div.matter01.mb > div.introduce'
+        ' > div.video-h.pl > h3'
+    )
     def parse(self, response):
-        for title in response.css('.post-header>h2'):
-            yield {'title': title.css('a ::text').get()}
+        for title in response.css(self.selector):
+            result = title.css('h3 ::text').get()
+            if result == "" or not result:
+                continue
+            yield {'title': result}
 
-        for next_page in response.css('a.next-posts-link'):
-            yield response.follow(next_page, self.parse)
+        for id in range(350, 465):
+            yield response.follow(f'http://pauuu.cn/nr.php?id={id}', self.parse)
