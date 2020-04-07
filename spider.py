b58f11e5a1be2b1ca763abd3aa81eaa779c8a340
Shenghan Gao <gaoshenghan199123@gmail.com>
2020-04-07 14:10:44 -0700

    one more scrap for today

--- spider.py
+++ spider.py
@@ -13,7 +13,7 @@ class BlogSpider(scrapy.Spider):
             result = title.css('h3 ::text').get()
             if result == "" or not result:
                 continue
-            yield {'title': result}
+            yield {'url': response.request.url, 'title': result, }
 
-        for id in range(350, 465):
+        for id in range(340, 470):
             yield response.follow(f'http://pauuu.cn/nr.php?id={id}', self.parse)
